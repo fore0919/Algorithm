aa85bc801f43ab11004d7906d3e24578bd091a37
fore0919 <gms02294@gmail.com>
2022-03-22 22:46:36 +0900

    MODIFY:체육복

--- programmers/lv.1/체육복.py
+++ programmers/lv.1/체육복.py
@@ -6,14 +6,16 @@ def solution1(n,lost,reserve):
     set_lost = set(lost) - set(reserve) # 잃어버렸지만 여벌이 있는 학생은 제외
     set_reserve = set(reserve) - set(lost) # 여벌이 있지만 잃어버려서 빌려줄 수 없는 학생은 제외
 
-    for i in set_reserve:
-        if i - 1 in set_lost:
-            set_lost.remove(i-1)
-        elif i + 1 in set_lost:
-            set_lost.remove(i+1)
-    return n-len(set_lost)
+    for i in set_reserve: # 여유분이 있는 학생들을 뽑음
+        if i - 1 in set_lost: # 왼쪽 학생이 잃어버렸는지 판단후 
+            set_lost.remove(i-1) # 잃어버렸다면 빌려주고 잃어버린 학생 리스트에서 삭제
+        elif i + 1 in set_lost: # 오른쪽 학생이 잃어버렸는지 판단후
+            set_lost.remove(i+1) # 잃어버렸다면 빌려주고 잃어버린 학생 리스트에서 삭제
+    return n-len(set_lost) # 전체학생에서 체육복을 빌리지 못한 인원 만큼 뺀 후 리턴
 
-#### 풀이 2 (짧은풀이)
+
+#### 풀이 2 (테스트 케이스 4개 실패)
+#### pythonic한 코드 이나 테스트 케이스 업데이트 이후 현재 통과 못함 
 def solution2(n, lost, reserve):
     reserve = set(reserve)
 
@@ -23,23 +25,25 @@ def solution2(n, lost, reserve):
 
     return n - len(lost)
 
-#### 풀이 3 (테스트 케이스 4개 실패)
-def solution(n,lost,reserve):
+#### 풀이 3 (테스트 케이스 4개 실패) 
+#### 수정 #### if문 -를 +위로 바꿈 왼쪽부터 빌려주도록 수정 후 테스트 통과 
+def solution3(n,lost,reserve):
     set_lost = set(lost) - set(reserve)
     set_reserve = set(reserve) - set(lost)
 
     for i in set_lost:
-        if i + 1 in set_reserve:
-            set_reserve.remove(i+1)
-        elif i - 1 in set_reserve:
+        if i - 1 in set_reserve:
             set_reserve.remove(i-1)
+        elif i + 1 in set_reserve:
+            set_reserve.remove(i+1)
         else:
             n -= 1
     return n
 
-#### 풀이 4 (테스트 케이스 2개 실패)
-def solution(n, lost, reserve): 
-    _reserve = [r for r in reserve if r not in lost] # 제한사항을 set 자료형으로 안하고 not in 컴프리헨션으로 처리
+#### 풀이 4 (테스트 케이스 2개 실패) 
+#### 수정 #### reserve정렬 이후 테스트 케이스 통과
+def solution4(n, lost, reserve): 
+    _reserve = sorted([r for r in reserve if r not in lost]) # 제한사항을 set 자료형으로 안하고 not in 컴프리헨션으로 처리
     _lost = [l for l in lost if l not in reserve]
 
     for r in _reserve:
@@ -52,4 +56,10 @@ def solution(n, lost, reserve):
     return n - len(_lost)
 
 
-
+n = 5
+lost = [2,4]
+reserve = [1,3,5]
+print (solution1(n,lost,reserve))
+print (solution2(n,lost,reserve))
+print (solution3(n,lost,reserve))
+print (solution4(n,lost,reserve))
