a7e2fbe63ed8e28816e5332944ad57a846d9a8db
fore0919 <gms02294@gmail.com>
2022-04-04 23:53:34 +0900

    ADD : recursion

--- etc/개미수열.py
+++ etc/개미수열.py
@@ -1,3 +1,5 @@
+import re
+
 # 초기값 : 3, 수열 개수 : 5 일때
 a = '3'
 b = ''
@@ -12,4 +14,16 @@ for i in range(0, 5):
     print("output:",b)
 
 
+# 재귀함수 이용 
+def gami(num):
+        string = gami(num-1)
+        result = ''
+        start = 0
+        while string:
+            last = re.search(string[0] + '+', string).end()
+            result = result + string[0] + str(last-start)
+            string = string[last:]
+        return result
 
+for i in range(1,6):
+    print (gami(i))
