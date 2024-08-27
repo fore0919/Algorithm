42ee689f8764697a55a85fa808bfe1ec2497e08e
fore0919 <gms02294@gmail.com>
2024-08-27 16:06:18 +0900

    Feat: temp

--- programmers/Lv.2/연습문제.py
+++ programmers/Lv.2/연습문제.py
@@ -559,3 +559,16 @@ def solution(storey):
             answer += x
         storey = storey // 10
     return answer
+
+
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/140107
+"""
+
+
+def solution(k, d):
+    answer = 0
+    for x in range(0, d + 1, k):
+        max_y = int((d**2 - x**2) ** 0.5)
+        answer += (max_y // k) + 1
+    return answer
