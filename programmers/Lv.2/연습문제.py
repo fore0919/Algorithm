32d32aba98ee7c1140c780139952cb71a41d0fae
fore0919 <gms02294@gmail.com>
2024-08-29 16:17:07 +0900

    feat: add lv3

--- programmers/Lv.2/연습문제.py
+++ programmers/Lv.2/연습문제.py
@@ -572,3 +572,74 @@ def solution(k, d):
         max_y = int((d**2 - x**2) ** 0.5)
         answer += (max_y // k) + 1
     return answer
+
+
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/135807
+"""
+
+
+def solution(arrayA, arrayB):
+
+    def gcd(array):
+        result = array[0]
+        for i in range(1, len(array)):
+            result = math.gcd(result, array[i])
+        return result
+
+    def func(gcd, arr):
+        for a in arr:
+            if a % gcd == 0:
+                return 0
+        return gcd
+
+    a = func(gcd(arrayA), arrayB)
+    b = func(gcd(arrayB), arrayA)
+    return max(a, b)
+
+
+from collections import deque
+
+dx, dy = [0, 0, -1, 1], [-1, 1, 0, 0]
+
+
+def bfs(maps, i, j, n, m, visit):
+    queue = deque()
+    queue.append((i, j))
+    visit[i][j] = 1
+    area = int(maps[i][j])
+    while queue:
+        x, y = queue.popleft()
+        for d in range(4):
+            nx, ny = x + dx[d], y + dy[d]
+            if (
+                0 <= nx < n
+                and 0 <= ny < m
+                and visit[nx][ny] == 0
+                and maps[nx][ny] != "X"
+            ):
+                visit[nx][ny] = 1
+                area += int(maps[nx][ny])
+                queue.append((nx, ny))
+    return area
+
+
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/154540
+"""
+
+
+def solution(maps):
+    n, m = len(maps), len(maps[0])
+    visit = [[0] * m for _ in range(n)]
+    answer = []
+
+    for i in range(n):
+        for j in range(m):
+            if maps[i][j] != "X" and visit[i][j] == 0:
+                answer.append(bfs(maps, i, j, n, m, visit))
+
+    if answer:
+        return sorted(answer)
+    else:
+        return [-1]
---
+++ programmers/Lv.3/야근지수.py
@@ -0,0 +1,39 @@
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/12927
+"""
+
+
+# 효율성 통과 실패
+def solution(n, works):
+    answer = 0
+    if sum(works) <= n:
+        return answer
+    while n:
+        works[works.index(max(works))] -= 1
+        n -= 1
+    return sum([work * work for work in works])
+
+
+# 효율성 통과 성공
+def solution(n, works):
+    answer = 0
+    if sum(works) <= n:
+        return 0
+
+    works.sort(reverse=True)
+    for i in range(n):
+        m = max(works)
+        j = 0
+        while 1:
+            if works[j] != m:
+                break
+            works[j] -= 1
+            n -= 1
+            j += 1
+            if n == 0:
+                break
+        if n == 0:
+            break
+    for work in works:
+        answer += work * work
+    return answer
