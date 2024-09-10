b2f29adae750207a012cf2f79a6354b38bad58eb
fore0919 <gms02294@gmail.com>
2024-09-10 14:35:26 +0900

    feat: add temp

--- programmers/Lv.3/KAKAO.py
+++ programmers/Lv.3/KAKAO.py
@@ -31,3 +31,30 @@ def solution(user_id, banned_id):
             answer.append(set(name))
 
     return len(answer)
+
+
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/67258
+"""
+
+from collections import defaultdict
+
+
+def solution(gems):
+    min_gems = int(1e9)
+    len_gems = len(gems)
+    n_gems = len(set(gems))
+    end = 0
+    temp = defaultdict(lambda: 0)
+    for start, gem in enumerate(gems):
+        while len(temp) < n_gems and end < len_gems:
+            temp[gems[end]] += 1
+            end += 1
+        if len(temp) == n_gems:
+            if min_gems > end - start:
+                min_gems = end - start
+                result = [start + 1, end]
+        temp[gem] -= 1
+        if temp[gem] == 0:
+            del temp[gem]
+    return result
