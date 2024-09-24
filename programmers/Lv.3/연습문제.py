f1f2eff937ef18062624daaa46e0630db2832ddc
fore0919 <gms02294@gmail.com>
2024-09-24 21:50:09 +0900

    feat: add graph

--- programmers/Lv.3/연습문제.py
+++ programmers/Lv.3/연습문제.py
@@ -57,3 +57,31 @@ def solution(n, s):
         else:
             answer.append(s // n)
     return sorted(answer)
+
+
+"""
+https://school.programmers.co.kr/learn/courses/30/lessons/49189?language=python3
+"""
+
+from collections import deque
+
+
+def solution(n, edge):
+    graph = [[] for _ in range(n + 1)]
+    distance = [-1] * (n + 1)
+
+    for e in edge:
+        graph[e[0]].append(e[1])
+        graph[e[1]].append(e[0])
+
+    q = deque([1])
+    distance[1] = 0
+
+    while q:
+        current = q.popleft()
+        for i in graph[current]:
+            if distance[i] == -1:
+                q.append(i)
+                distance[i] = distance[current] + 1
+
+    return distance.count(max(distance))
