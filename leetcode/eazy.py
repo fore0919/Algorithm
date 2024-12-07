dd34edd6798352aa1a764d67498d669470ed9c15
fore0919 <gms02294@gmail.com>
2024-12-07 19:28:53 +0900

    feat: add leetcode

--- leetcode/eazy.py
+++ leetcode/eazy.py
@@ -1,5 +1,5 @@
 from collections import Counter
-from typing import List
+from typing import List, Optional
 
 
 class Solution:
@@ -76,3 +76,87 @@ class Solution:
         if string[: (n // 2) + (n % 2)] == "".join(reversed(string[n // 2 :])):
             return True
         return False
+
+    """
+    383. Ransom Note
+    """
+
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        for i in list(set(ransomNote)):
+            if ransomNote.count(i) > magazine.count(i):
+                return False
+        return True
+
+    """
+    228. Summary Ranges
+    """
+
+    def summaryRanges(self, nums: List[int]) -> List[str]:
+        answer = []
+        if not nums:
+            return answer
+        start = nums[0]
+        for i in range(1, len(nums) + 1):
+            if i == len(nums) or nums[i] != nums[i - 1] + 1:
+                if start == nums[i - 1]:
+                    answer.append(str(start))
+                else:
+                    answer.append(f"{start}->{nums[i - 1]}")
+                if i < len(nums):
+                    start = nums[i]
+        return answer
+
+    """
+    20. Valid Parentheses
+    """
+
+    def isValid(self, s: str) -> bool:
+        stack = []
+        hash_map = {")": "(", "}": "{", "]": "["}
+        for i in s:
+            if i in hash_map.values():
+                stack.append(i)
+            elif i in hash_map.keys():
+                if not stack or hash_map[i] != stack.pop():
+                    return False
+        return True if not stack else False
+
+    """
+    Linked List Cycle
+    """
+
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+
+class Solution3:
+    def hasCycle(self, head: Optional[ListNode]) -> bool:
+        fast = head
+        slow = head
+        while fast and fast.next:
+            fast = fast.next.next
+            slow = slow.next
+            if fast == slow:
+                return True
+        return False
+
+    """
+    104. Maximum Depth of Binary Tree
+    """
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution4:
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        if not root:
+            return 0
+        return 1 + max(self.maxDepth(root.left), self.mexDepth(root.right))
