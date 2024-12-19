0fbf43dd89d1ee8895c58826afd55521c28040eb
fore0919 <gms02294@gmail.com>
2024-12-19 14:09:36 +0900

    feat: leetcode 12/19

--- leetcode/medium.py
+++ leetcode/medium.py
@@ -297,3 +297,47 @@ class Solution3:
                     answer += 1
                     bfs(i, j)
         return answer
+
+    """
+    120. Triangl
+    """
+
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
+        dp = [[0] * n for _ in range(n)]
+        dp[n - 1] = triangle[n - 1]
+        for i in range(n - 2, -1, -1):
+            for j in range(i + 1):
+                right = triangle[i][j] + dp[i + 1][j]
+                left = triangle[i][j] + dp[i + 1][j + 1]
+                dp[i][j] = min(right, left)
+        return dp[0][0]
+
+    def minimumTotal2(self, triangle: List[List[int]]) -> int:
+        memo = [[-1] * len(triangle) for _ in range(len(triangle))]
+
+        def dfs(i, j):
+            if i == len(triangle):
+                return 0
+            if memo[i][j] != -1:
+                return memo[i][j]
+
+            lower_left = triangle[i][j] + dfs(i + 1, j)
+            lower_right = triangle[i][j] + dfs(i + 1, j + 1)
+            memo[i][j] = min(lower_left, lower_right)
+            return memo[i][j]
+
+        return dfs(0, 0)
+
+    """
+    71. Simplify Path
+    """
+
+    def simplifyPath(self, path: str) -> str:
+        arr = []
+        for s in path.split("/"):
+            if arr and s == "..":
+                arr.pop()
+            elif s not in [".", "", ".."]:
+                arr.append(s)
+        return "/" + "/".join(arr)
